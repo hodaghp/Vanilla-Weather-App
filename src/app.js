2a7733051c62f55f475478656297eb3127cbc630
Hoda <hoda.ghp@gmail.com>
2022-12-22 03:16:27 +0330

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,19 +1,43 @@
+function formatDate(timestemp) {
+  let date = new Date(timestemp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}  ${hour}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let city = response.data.name;
   let weatherDiscription = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
+  let data = formatDate(response.data.dt * 1000);
   document.querySelector("#current-temperature").innerHTML = temperature;
   document.querySelector("#city").innerHTML = city;
   document.querySelector("#description").innerHTML = weatherDiscription;
   document.querySelector("#humidity").innerHTML = humidity;
   document.querySelector("#wind").innerHTML = wind;
+  document.querySelector("#date").innerHTML = data;
 }
 
 let apiKey = "3ee42c7bb4064d140b4cd8b3cb917db0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-//let time = document.querySelector("#date");
-// time.innerHTML = formatDate();
 axios.get(apiUrl).then(displayTemperature);
